7b03b4972df5d1ad31b3b5058066e1315fb9c1aa
redouane karzazi <redouane.karzazi@xelops.com>
2024-10-08 12:07:27 +0100

    resolve deprecated methods

--- src/main/kotlin/com/helpers/flywayhelper/utils/terminal/TerminalClient.kt
+++ src/main/kotlin/com/helpers/flywayhelper/utils/terminal/TerminalClient.kt
@@ -4,6 +4,7 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.guessProjectDir
 import com.intellij.vcsUtil.VcsUtil
 import java.io.File
+import java.util.*
 
 
 class TerminalClient(private val project: Project){
@@ -12,15 +13,29 @@ class TerminalClient(private val project: Project){
 
 
     fun exec(command: String): List<String> {
-        val process = Runtime.getRuntime().exec(command, null, projectDir)
+
+        val process = Runtime.getRuntime().exec(tokenize(command), null, projectDir)
         return process.inputStream.bufferedReader().use { it.readLines() }
     }
 
     fun execVoid(command: String, waitFor: Boolean) {
-        val process = Runtime.getRuntime().exec(command, null, projectDir)
+        val process = Runtime.getRuntime().exec(tokenize(command), null, projectDir)
         if (waitFor) {
             process.waitFor()
         }
     }
 
+    private fun tokenize(command: String): Array<String?> {
+
+        val st = StringTokenizer(command)
+        val cmdArray = arrayOfNulls<String>(st.countTokens())
+        var i = 0
+        while (st.hasMoreTokens()) {
+            cmdArray[i] = st.nextToken()
+            i++
+        }
+        return cmdArray
+    }
+
 }
+
